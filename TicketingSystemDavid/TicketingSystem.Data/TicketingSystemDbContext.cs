0b5d87a32e386a63e60f955264b1aa2e53bfc1d3
Mircho Mirchev <Mircho.Mirchev98@gmail.com>
2023-10-04 18:38:41 +0300

    Created Architecture and DbContext

---
+++ TicketingSystemDavid/TicketingSystem.Data/TicketingSystemDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketingSystem.Data.Models;
+
+namespace TicketingSystem.Data
+{
+    public class TicketingSystemDbContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
+    {
+    }
+}
